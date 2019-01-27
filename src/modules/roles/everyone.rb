3147000e1dc250b804bad778a385791baafe9eac
MMay1818 <39638156+MMay1818@users.noreply.github.com>
2019-01-26 23:23:49 -0600

    Update everyone.rb

--- src/modules/roles/everyone.rb
+++ src/modules/roles/everyone.rb
@@ -2,6 +2,6 @@ module Bot::DiscordRoles
   module Everyone
     extend Discordrb::Commands::CommandContainer
 
-    Bot::BOT.set_role_permission(364123620659822602, 1)
+    Bot::BOT.set_role_permission(412089660953722883, 1)
   end
 end
