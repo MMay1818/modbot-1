f0d8399e0d1cb9d018a498807f0b2fda71212cb1
MMay1818 <39638156+MMay1818@users.noreply.github.com>
2019-01-26 23:24:02 -0600

    Update warden.rb

--- src/modules/roles/warden.rb
+++ src/modules/roles/warden.rb
@@ -2,6 +2,6 @@ module Bot::DiscordRoles
   module Warden
     extend Discordrb::Commands::CommandContainer
 
-    Bot::BOT.set_role_permission(382257109854715906, 4)
+    Bot::BOT.set_role_permission(412089660953722883, 4)
   end
 end
