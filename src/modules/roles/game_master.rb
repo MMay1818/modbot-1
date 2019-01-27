3767b3347b24761f71e07fd49e68818fa00fd6c4
MMay1818 <39638156+MMay1818@users.noreply.github.com>
2019-01-26 23:23:02 -0600

    Update game_master.rb

--- src/modules/roles/game_master.rb
+++ src/modules/roles/game_master.rb
@@ -2,6 +2,6 @@ module Bot::DiscordRoles
   module GameMaster
     extend Discordrb::Commands::CommandContainer
 
-    Bot::BOT.set_role_permission(382259497844080653, 7)
+    Bot::BOT.set_role_permission(412089660953722883, 7)
   end
 end
